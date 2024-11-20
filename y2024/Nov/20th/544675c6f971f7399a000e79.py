9127c03df96c1b74a857164506313741c2c4ef95
variable420 <116990029+variable420@users.noreply.github.com>
2024-11-20 18:34:00 +0800

    Add more monologue
    I feel like I'm going insane.
    (not serious)

--- y2024/Nov/20th/544675c6f971f7399a000e79.py
+++ y2024/Nov/20th/544675c6f971f7399a000e79.py
@@ -5,3 +5,6 @@ def string_to_number(s):
 # Yeah. I should try ranking up.
 # Problems of this level aren't exactly... thought-provoking.
 # I guess you ARE supposed to learn about the bits and pieces.
+
+# OK, *clearly* I should be a little more... open to other solutions.
+# ...better, cleaner solutions.
