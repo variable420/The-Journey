2d7f6c5445abafd4696fc10aac2b7b9d77c00683
variable420 <116990029+variable420@users.noreply.github.com>
2024-11-20 18:30:44 +0800

    6th kata

---
+++ y2024/Nov/20th/544675c6f971f7399a000e79.py
@@ -0,0 +1,7 @@
+def string_to_number(s):
+    n = int(s)
+    return n
+
+# Yeah. I should try ranking up.
+# Problems of this level aren't exactly... thought-provoking.
+# I guess you ARE supposed to learn about the bits and pieces.
